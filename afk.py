e355c3a80f4acdafa2ae2a187dbde2a91d33fa78
memetron5000 <70104043+memetron5000@users.noreply.github.com>
2023-01-08 20:56:11 -0500

    Update afk.py

--- afk.py
+++ afk.py
@@ -4,17 +4,18 @@ import random
 import time
 import sys
 a=["a","s","d","w"]
-c=True
+
 while True:
-    if(keyboard.read_key("-")):
-        while c==True:
+    if(keyboard.read_key("'a")):
+        while True:
             if (keyboard.is_pressed("+")):
                 sys.exit(0)
             else:
                 b=random.choice(a)
                 inpu.keyDown(b)
-                time.sleep(1)
+                time.sleep(0.01)
                 inpu.keyUp(b)
+                time.sleep(5)
             
                 
             
