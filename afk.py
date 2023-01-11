ebcf7656883237dfc89067e46773ce9fc171c9a9
memetron5000 <70104043+memetron5000@users.noreply.github.com>
2023-01-10 22:18:33 -0500

    Update afk.py

--- afk.py
+++ afk.py
@@ -11,14 +11,14 @@ a=["a","s","d","w"]
 def breakout():
     while True:
         if (keyboard.is_pressed("+")):
-            sys.exit(randomizer) 
+            sys.exit(randomizer) #parte a remplazar que rompa la funcion
 def randomizer():
     while True:
         if(keyboard.read_key("-")):
             while True:
                 b=random.choice(a)
                 inpu.keyDown(b)
-                time.sleep(1)
+                time.sleep(random.randrange(2,5))
                 inpu.keyUp(b)
 
 
@@ -27,7 +27,6 @@ p2 = threading.Thread(target=breakout)
 
 p1.start()
 p2.start()
-            
             
                 
             
